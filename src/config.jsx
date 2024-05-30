ef3b0f30782d8a6eedd5c56534328098843dc942
montassar tayachi <montassartayachi2002@gmail.com>
2024-05-30 18:15:41 +0100

    update

--- src/config.jsx
+++ src/config.jsx
@@ -1,4 +1,4 @@
 // // apiConfig.js
 // https://mall-shopping.onrender.com
 // http://localhost:4000
-export const API_BASE_URL = 'http://localhost:4000'
+export const API_BASE_URL = 'https://swiftcart-bakend.onrender.com'
